9b29092f1c32a7bbd03c0e457284d5c2ce1d2e46
Xiangyang Ju <xiangyang.ju@gmail.com>
2023-04-25 14:14:06 -0700

    import the task base config

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='hydra_tasks',
+      version='1.0.0',
+      description='',
+      url='https://github.com/xju2/hydra_template',
+      author='Xiangyang Ju',
+      author_email='xiangyang.ju@gmail.com',
+      license='MIT',
+      packages=['src'],
+      zip_safe=False,
+      include_package_data=True
+      )
---
+++ src/utils/__init__.py
@@ -0,0 +1,9 @@
+from src.utils.pylogger import get_pylogger
+from src.utils.rich_utils import enforce_tags, print_config_tree
+from src.utils.utils import (
+    close_loggers,
+    extras,
+    save_file,
+    task_wrapper,
+    instantiate_tasks,
+)
